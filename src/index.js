a1eaac26610e801382dc9a8985b598664e7fa746
Alejandro Morgan <MY_NAME@example.com>
2025-07-04 18:20:32 -0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -74,6 +74,6 @@ app.get('/', (req, res) => {
 
 // === INICIO DE SERVIDOR ===
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
